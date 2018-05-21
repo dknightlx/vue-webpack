e173110132dfe5c61f535ac24895fc288dfde30d
DK <liuxu@souche.com>
2018-05-21 15:59:05 +0800

    space 2 => 4

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="app">
-    {{#router}}
-    <router-view/>
-    {{else}}
-    <HelloWorld/>
-    {{/router}}
-  </div>
+    <div id="app">
+        {{#router}}
+        <router-view/>
+        {{else}}
+        <HelloWorld/>
+        {{/router}}
+    </div>
 </template>
 
 <script>
@@ -14,20 +14,20 @@ import HelloWorld from './components/HelloWorld'{{#if_eq lintConfig "airbnb"}};{
 
 {{/unless}}
 export default {
-  name: 'app'{{#router}}{{#if_eq lintConfig "airbnb"}},{{/if_eq}}{{else}},
-  components: {
-    HelloWorld{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-  }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}{{/router}}
+    name: 'app'{{#router}}{{#if_eq lintConfig "airbnb"}},{{/if_eq}}{{else}},
+    components: {
+      HelloWorld{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+    }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}{{/router}}
 }{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
--- template/src/router/index.js
+++ template/src/router/index.js
@@ -5,11 +5,11 @@ import Index from '../pages/'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 Vue.use(Router){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Index',
-      component: Index{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-    }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-  ]{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+    routes: [
+        {
+            path: '/',
+            name: 'Index',
+            component: Index{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+        }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+    ]{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
 }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
