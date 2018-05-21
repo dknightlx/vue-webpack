8409a17fe9fafc6c7e38022237cb0853ef10814b
DK <liuxu@souche.com>
2018-05-21 15:52:39 +0800

    add pages

---
+++ template/src/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="component-container"></div>
+</template>
+
+<script>
+import Hello from '../components/HelloWorld';
+
+export default {
+    components: {
+        Hello
+    }
+};
+</script>
+
+<style lang="less" scoped>
+
+</style>
